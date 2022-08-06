084d249dc5461b69d27fba1b72249caff6bce545
Caio Fontes <olixis@gmail.com>
2022-08-05 23:54:55 -0300

    Update puppeteer.js

--- puppeteer.js
+++ puppeteer.js
@@ -5,9 +5,14 @@ const runner = async search => {
   console.log("Opening browser");
   const browser = await puppeteer.launch({
     headless: false,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    args: ["--no-sandbox", "--disable-setuid-sandbox",'--start-maximized']
   });
   const page = await browser.newPage();
+  
+  await page.setViewport({
+    width: 1920,
+    height: 1080
+  });
 
   async function cleanup() {
     try {
@@ -18,17 +23,17 @@ const runner = async search => {
       console.log("Cannot cleanup istances");
     }
   }
-
+  let resp = "";
   try {
     console.log("Navigating url");
     await page.goto(search, { waitUntil: "networkidle2" });
+    resp = await page.content()
     await cleanup();
   } catch (e) {
     console.error("Error happened", e);
-    await page.screenshot({ path: "error.png" });
     await cleanup();
   }
-  return await page.content();
+  return resp;
 };
 
 module.exports = runner;
